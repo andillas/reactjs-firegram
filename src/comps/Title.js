443f3226a69df9ddbbf8dada33caf448c887ca41
Eugenio <andillas@gmail.com>
2022-02-03 22:34:48 +0100

    Firebase connected and working. Ready to continue with "progressbar"

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {collection, getDocs} from 'firebase/firestore';
+import {projectFirestore} from '../firebase/config';
 
 const Title = () => {
+
+useEffect(()=>{
+  const obtenerDatos = async ()=>{
+    const datos = await getDocs(collection(projectFirestore, "usuarios"));
+    datos.forEach(col => {
+      console.log(col.data())
+    })
+    
+  }
+  //obtenerDatos();
+}, [])
+
+
   return (
     <div className="title">
       <h1>FireGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>These are your pictures. Enjoy'em.</p>
     </div>
   )
 }
